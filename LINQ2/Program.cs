22489205f51c88c892ba4757339dc37574fdaf1a
estimon <33309282+estimon@users.noreply.github.com>
2018-02-26 22:28:01 +0200

    Initial commit

---
+++ LINQ2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esimene:");
+            Console.WriteLine("------------------------------------");
+
+            int WordsCount = 0;
+            string Path = @"C:\games\tekst.txt";
+            string Text = new System.IO.StreamReader(Path).ReadToEnd();
+            WordsCount = Text.Split(' ').Length;
+            Console.WriteLine("Tekstis on " + WordsCount + " sõna");
+
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Teine ja kolmas:");
+            Console.WriteLine("------------------------------------");
+
+            var counts = Text.GroupBy(x => x).OrderBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var item in counts)
+            {
+                Console.WriteLine("Tekstis on: " + item.Key + " " + item.Value + " tükki");
+            }
+
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Neljas:");
+            Console.WriteLine("------------------------------------");
+
+
+
+ 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LINQ2/Tähed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ2
+{
+    class Tähed
+    {
+        public static SortedDictionary<char, ulong> Count(string stringToCount)
+        {
+            SortedDictionary<char, ulong> characterCount = new SortedDictionary<char, ulong>();
+
+            foreach (var character in stringToCount)
+            {
+                if (!characterCount.ContainsKey(character))
+                {
+                    characterCount.Add(character, 1);
+                }
+                else
+                {
+                    characterCount[character]++;
+                }
+            }
+
+            return characterCount;
+        }
+    }
+}
